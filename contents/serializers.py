e2f032f04cb06e1eeb16ddc5786c096a4b7e14a1
Sergio Garcia Campderrich <gio8.tisu@gmail.com>
2022-05-31 18:11:08 +0200

    Show channels content on endpoint

--- contents/serializers.py
+++ contents/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 
-from .models import Channel
+from .models import Channel, Content
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    authors = serializers.SlugRelatedField(
+        slug_field="full_name", read_only=True, many=True
+    )
+
+    class Meta:
+        model = Content
+        fields = ["id", "file", "rating", "genre", "authors"]
 
 
 class ChannelSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
     class Meta:
         model = Channel
-        fields = ["id", "title", "language"]
+        fields = ["id", "title", "language", "contents"]
