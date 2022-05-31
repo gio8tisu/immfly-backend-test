7fee2c5208b2021c1dad3584670ba548a0f71cc9
Sergio Garcia Campderrich <gio8.tisu@gmail.com>
2022-05-31 13:35:27 +0200

    Add Content admin page and inline

--- contents/admin.py
+++ contents/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Channel
+from .models import Channel, Content
+
+
+class ContentInline(admin.TabularInline):
+    model = Content
 
 
 @admin.register(Channel)
@@ -12,3 +16,15 @@ class ChannelAdmin(admin.ModelAdmin):
         )
     ]
     list_display = ["id", "title"]
+    inlines = [ContentInline]
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            "Content information",
+            {"fields": ["rating", "channel"]},
+        )
+    ]
+    list_display = ["id", "rating", "channel"]
