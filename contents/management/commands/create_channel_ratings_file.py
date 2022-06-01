c1baad6b1180ed359d804bb84226e36ac3ba442a
Sergio Garcia Campderrich <gio8.tisu@gmail.com>
2022-06-01 15:29:56 +0200

    Change compute score management command to output rows sorted by rating

--- contents/management/commands/create_channel_ratings_file.py
+++ contents/management/commands/create_channel_ratings_file.py
@@ -1,9 +1,13 @@
 import csv
+import logging
+from operator import itemgetter
 
 from django.core.management.base import BaseCommand
 
 from contents.models import Channel
 from contents.services import compute_channel_rating
+
+logger = logging.getLogger("main")
 
 
 class Command(BaseCommand):
@@ -16,8 +20,16 @@ class Command(BaseCommand):
         with open(options["output_file"], "w", newline="") as fd:
             fieldnames = ["channel_id", "channel_rating"]
             writer = csv.DictWriter(fd, fieldnames=fieldnames)
-
             writer.writeheader()
+
+            logger.info("Computing channel ratings")
+
+            ratings = []
             for channel in Channel.objects.all():
                 rating = compute_channel_rating(channel)
-                writer.writerow({"channel_id": channel.id, "channel_rating": rating})
+                ratings.append({"channel_id": channel.id, "channel_rating": rating})
+
+            ratings.sort(key=itemgetter("channel_rating"), reverse=True)
+
+            logger.info("Writing CSV file")
+            writer.writerows(ratings)
