fef4a661fa15c086fae4e399c1e906727a96646d
Sergio Garcia Campderrich <gio8.tisu@gmail.com>
2022-06-01 10:41:04 +0200

    Add custom manager to channel model for creation

--- contents/models.py
+++ contents/models.py
@@ -1,10 +1,32 @@
 import uuid
 
+from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
+class ChannelManager(models.Manager):
+    def create_channel(self, title, language, picture, subchannels, contents):
+        if subchannels and contents:
+            raise ValidationError("Channel cannot have both subchannels and contents")
+        if not subchannels and not contents:
+            raise ValidationError("Channel must have subchannels or contents")
+
+        new_channel = Channel(title=title, language=language, picture=picture)
+        new_channel.full_clean()
+        new_channel.save()
+
+        if subchannels:
+            new_channel.subchannels.add(*subchannels)
+        if contents:
+            new_channel.contents.add(*contents)
+
+        return new_channel
+
+
 class Channel(models.Model):
+    objects = ChannelManager()
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=128)
     language = models.ForeignKey("ChannelLanguage", on_delete=models.PROTECT)
