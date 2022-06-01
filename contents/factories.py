1ac635c32a010f7def229f3dc27a70c0a55d616f
Sergio Garcia Campderrich <gio8.tisu@gmail.com>
2022-06-01 12:11:41 +0200

    Refactor tests using improved factories

--- contents/factories.py
+++ contents/factories.py
@@ -4,19 +4,16 @@ from .models import Author, Channel, ChannelLanguage, Content, ContentGenre
 
 
 class ChannelLanguageFactory(factory.django.DjangoModelFactory):
+    language = factory.Faker("language_name")
+    code = factory.Faker("language_code")
+
     class Meta:
         model = ChannelLanguage
 
 
-class ChannelFactory(factory.django.DjangoModelFactory):
-    title = "TV Shows"
-    language = factory.SubFactory(ChannelLanguageFactory)
-
-    class Meta:
-        model = Channel
-
-
 class ContentGenreFactory(factory.django.DjangoModelFactory):
+    name = factory.Faker("word")
+
     class Meta:
         model = ContentGenre
 
@@ -43,3 +40,23 @@ class ContentFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Content
+
+
+class ChannelFactory(factory.django.DjangoModelFactory):
+    title = "TV Shows"
+    language = factory.SubFactory(ChannelLanguageFactory)
+    picture = factory.django.ImageField()
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        """Override the default ``_create`` with our custom call."""
+        contents = kwargs.pop("contents", None)
+        subchannels = kwargs.pop("subchannels", None)
+
+        manager = cls._get_manager(model_class)
+        return manager.create_channel(
+            contents=contents, subchannels=subchannels, **kwargs
+        )
+
+    class Meta:
+        model = Channel
